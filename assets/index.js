5917a3ecb3066132289f46ac16a9e0cd6c5a1beb
jayeshcspl <jayesh@complitech.net>
2022-12-31 18:04:14 +0530

    cookie and new design homepage changes

--- assets/index.js
+++ assets/index.js
@@ -176,11 +176,12 @@ jQuery('.dot_img').each(function( index ) {
       // $('.l-left svg').attr("viewBox", "0 0 320 68.4"); 
     } else {
       $('.header-top').removeClass('stuck');
-      if($('body#store-locator').length){
-        $('.header-top').css({"background-color":"#ffffff"});
-      } else {
-        $('.header-top').css({"background-color":"transparent"});
-      }
+      //if($('body#store-locator').length || $('body.template-index').length){
+      // if($('body#store-locator').length){
+      //   $('.header-top').css({"background-color":"#ffffff"});
+      // } else {
+      //   $('.header-top').css({"background-color":"transparent"});
+      // }
       $('.james').show();
       $('.ays').hide();
       // $('.l-left svg').attr("viewBox", "0 0 575 68.4");
